321b9fca1b9a294ddbc0e9234a4746e1aec243ae
Isabela Andrade <166050128+belaandradeb@users.noreply.github.com>
2025-11-18 09:50:50 -0300

    qqq

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,10 @@
 function App () {
-  const[message, setMessage] = useState('Olá, mundo!')
+  const[message, setMessage] = useState('Olá, mundo!');
+  return(
+    <div>
+      <h1>{message}</h1>
+      <button onClick={()=>{setMessage('olá fui clicado');}}></button>
+    </div>
+  )
 }
+export default App
