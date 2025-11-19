101194146cb845a0e52556ea67f549ea26f96253
Isabela Andrade <166050128+belaandradeb@users.noreply.github.com>
2025-11-19 09:30:52 -0300

    qqqq

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
+import { useState } from "react";
 function App () {
   const[message, setMessage] = useState('Olá, mundo!');
   return(
     <div>
       <h1>{message}</h1>
-      <button onClick={()=>{setMessage('olá fui clicado');}}></button>
+      <button onClick={()=>{setMessage('olá fui clicado');}}>botão</button>
     </div>
   )
 }
